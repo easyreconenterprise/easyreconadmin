9ea19fda2caf8ee72f57024832471d2794abe11f
olaniyihope2 <olaniyihope2@gmail.com>
2024-12-17 15:17:14 +0100

    all

--- src/app/views/dashboard/import/UnmappedData.js
+++ src/app/views/dashboard/import/UnmappedData.js
@@ -12,62 +12,6 @@ import { Link } from "react-router-dom";
 import { SessionContext } from "app/components/MatxLayout/SwitchContext";
 // import FinancialSummary from './FinancialSummary'
 
-const Subcategory = {
-  Asset: {
-    "Non-current": {
-      "Property plant and equipment": [],
-      "Deferred tax assets": [],
-      Investment: [],
-      "Other receivables": [],
-      "Intangible assets and good will": [],
-      "Biological assets": [],
-      "Investment property": [],
-    },
-    Current: {
-      Inventories: [],
-      "Trade and other receivables": [],
-      "prepayment and advances": [],
-      "Cash and cash equivalent": [],
-    },
-  },
-  "Equity and Liability": {
-    "Non-current-Liability": {
-      "Deferred Income": [],
-      Provision: [],
-      "Deferred tax liability": [],
-      "Trade and other payables": [],
-      "Loans and borrowings": [],
-    },
-
-    "Current-liability": {
-      "Bank Overdraft": [],
-      "Current tax liabilities": [],
-      "Deferred Incomes": [],
-      "Loans and borrowing": [],
-      "Trade and other payable": [],
-    },
-    Equity: {
-      "Capital and reserves": [],
-      "Share capital": [],
-      "Retained earnings": [],
-      " Reserves": [],
-      "Share Premium": [],
-    },
-  },
-  Income: {
-    Revenue: [],
-    "Cost of sales": [],
-    "Other income": [],
-    "Administrative,  and selling expenses": [],
-    "Impairment loss on trade receivables": [],
-    "Finance income": [],
-    "Finance cost": [],
-    "Minimum tax expense": [],
-    Taxation: [],
-    "Loss from discontinued operations": [],
-  },
-};
-
 const UnMappedData = () => {
   const [mappedData, setMappedData] = useState([]); // State to manage mapped data
 
